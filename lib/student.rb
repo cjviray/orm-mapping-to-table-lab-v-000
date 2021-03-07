5d290ebc70b06103433e58056004a1bbaba936d7
Christian V <viray.christianj@gmail.com>
2021-03-07 07:51:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
     
     DB[:conn].execute(sql, self.name, self.grade)
     
-    @id = 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
   def self.create(name:, grade:)
